8fe2b44256c6cc9f86f60a2ee735c4bf49b5dbde
kang <rkdalstb044@gmail.com>
2025-06-05 16:42:17 +0900

    ✨ 입금 기능 추가
    사용자가 입금 금액을 입력하고, 잔액이 업데이트 되도록 기능 구현

--- atm.py
+++ atm.py
@@ -1,19 +1,24 @@
-# 잔액 초기화
+# 잔액 초기화, 통장 잔고
 balance = 1000
 
 while True:# 조건이 True면 계속 작동합니다.
     num = input('사용하실 번호을 선택해주세요 (1.입금, 2.출금, 3.영수증보기, 4.종료): ')
-
+    #2번 출금 기능 코드
     if num == '2':
         print('출금')
-
+    #1번 입금 기능 코드
     if num == '1':
-        print('입금')
+        #얼마를 입금할거야?
+        deposit_amount = int(input("입금할 금액을 입력해주세요 : "))
+        #balance
+        balance += deposit_amount
+        print(f'입금하신 금액은 {deposit_amount}원 이고, 현재 잔액은 {balance}원 입니다')
 
+    #4번 종료 기능 코드
     if num == '4':
         print('종료')
         break
-
+    #3번 영수증 보기 코드
     if num == '3':
         print('영수증보기')
 # 목차 = {"1.입금", "2.출금", "3.영수증 보기", "4.종료"}
